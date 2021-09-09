d485085d42ba354d28b02043ea412700ad04b3e1
Diego Barría <dbarriapaiva@gmail.com>
2021-09-09 13:28:24 -0300

    Better preferences

--- Tweak/Sources/Mochi/Preferences.swift
+++ Tweak/Sources/Mochi/Preferences.swift
@@ -1,11 +1,23 @@
 import Foundation
-import CoreGraphics
 
+struct Prefs: Codable {
+    var scrollYOffset: Double = 0
+}
+
+func loadPrefs(identifier: String) -> Prefs? {
+    guard let plistFile = FileManager.default.contents(atPath: identifier) else {
+        NSLog("[Mochi] Preferences config not found")
+        return nil
+    }
+    
+    let decoder = PropertyListDecoder()
+    return try? decoder.decode(Prefs.self, from: plistFile)
+}
 
 class Preferences {
 
     var identifier: String = ""
-    var yOffset: CGFloat = 0
+    var yOffset: Double = 0
 
     init(identifier: String) {
         var propertyListFormat =  PropertyListSerialization.PropertyListFormat.xml //Format of the Property List.
@@ -21,7 +33,7 @@ class Preferences {
             plistData = try PropertyListSerialization.propertyList(from: plistXML, options: .mutableContainersAndLeaves, format: &propertyListFormat) as! [String:AnyObject]
 
             if let yOffset = plistData["yOffset"] {
-                self.yOffset = (yOffset as? CGFloat ?? 0) ?? 0
+                self.yOffset = (yOffset as? Double ?? 0)
             }
         } catch {
             NSLog("orion Error reading plist: \(error), format: \(propertyListFormat)")
