8e264939d274b0c8d743748658b95acaeb56ba28
Diego Barría <dbarriapaiva@gmail.com>
2021-09-16 19:30:48 -0300

    works on arm64e

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 export ARCHS = arm64 arm64e
-export SYSROOT = $(THEOS)/sdks/iPhoneOS13.0.sdk
-export PREFIX=$(THEOS)/toolchain/Xcode.xctoolchain/usr/bin/
-export TARGET = iphone:clang:14.5:13.0
+export SYSROOT = $(THEOS)/sdks/iPhoneOS14.4.sdk
+#export PREFIX= $(THEOS)/toolchain/Xcode.xctoolchain/usr/bin/
+export TARGET = iphone:clang::14.0
 
 INSTALL_TARGET_PROCESSES = SpringBoard
 
--- Tweak/Sources/Kori/Hooks/Tweak.x.swift
+++ Tweak/Sources/Kori/Hooks/Tweak.x.swift
@@ -18,49 +18,6 @@ class CoverSheetController : ClassHook<CSCoverSheetViewController> {
     }
 }
 
-// Notifications Scroll view hook
-class NotificationsHook : ClassHook<UIView> {
-
-    static let targetName: String = "CSCoverSheetViewBase"
-    
-    @Property(.nonatomic) var originalFrame = CGRect()
-
-    func didMoveToWindow() {
-        orig.didMoveToWindow()
-        let ancestor: UIViewController = target._viewControllerForAncestor()!
-        if(ancestor.isKind(of: CSCombinedListViewController.self)) {
-            Manager.sharedInstance.notificationsView = target
-        }
-    }
-
-    func setFrame(_ frame: CGRect) {
-        
-        // We only want to update one CSCoverSheetViewBase
-        let ancestor: UIViewController? = target._viewControllerForAncestor()
-
-        if(ancestor != nil && ancestor!.isKind(of: CSCombinedListViewController.self)) {
-            
-            var newFrame = frame
-            if(!Manager.sharedInstance.isEditing) {
-                originalFrame = frame
-                newFrame = frame
-            } else {
-                newFrame = originalFrame
-            }
-
-            newFrame.origin.y += Manager.sharedInstance.notificationsYOffset
-            newFrame.origin.x += Manager.sharedInstance.notificationsXOffset
-            newFrame.size.width += Manager.sharedInstance.notificationsWidthOffset
-            newFrame.size.height += Manager.sharedInstance.notificationsHeightOffset
-            orig.setFrame(newFrame)
-        
-        } else {
-            orig.setFrame(frame)
-        }
-    }
-}
-
-
 struct Kori : Tweak {
     init() {
         CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(),
