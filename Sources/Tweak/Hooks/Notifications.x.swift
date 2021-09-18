4b1cdfd012df820ee98538e5f83f42528bb031ba
Diego Barría <dbarriapaiva@gmail.com>
2021-09-18 16:06:13 -0300

    Gradient works better now && all notification offsets are in one hook now

--- Sources/Tweak/Hooks/Notifications.x.swift
+++ Sources/Tweak/Hooks/Notifications.x.swift
@@ -2,7 +2,7 @@ import TweakC
 import Orion
 import UIKit
 
-// Notifications Scroll view hook (Works for X, Y && width offset)
+// Notifications Scroll view hook
 class NotificationsHook : ClassHook<UIView> {
 
     static let targetName: String = "CSCoverSheetViewBase"
@@ -33,6 +33,10 @@ class NotificationsHook : ClassHook<UIView> {
             newFrame.origin.y += Manager.sharedInstance.notificationsYOffset
             newFrame.origin.x += Manager.sharedInstance.notificationsXOffset
             newFrame.size.width += Manager.sharedInstance.notificationsWidthOffset
+            
+            target.clipsToBounds = Manager.sharedInstance.notificationsHeightOffset != 0
+            newFrame.size.height += Manager.sharedInstance.notificationsHeightOffset
+           
             orig.setFrame(newFrame)
 
         } else {
@@ -41,87 +45,42 @@ class NotificationsHook : ClassHook<UIView> {
     }
 }
 
-// Notifications Scroll view hook
-// (Works only for height offset, basically because media player dont subview this class)
-class NotificationList : ClassHook<NCNotificationListView> {
+// We just add gradient here
+class NotificationStructuredListHook : ClassHook<NCNotificationStructuredListViewController> {
+
+    @Property(.nonatomic) var gradientLayer = CAGradientLayer()
     
-    @Property(.nonatomic) var originalFrame = CGRect()
-    @Property(.nonatomic) var isTargetView = false
-    @Property(.nonatomic) var gradientLayer: CAGradientLayer? = nil
-    
-    func didMoveToWindow() {
-        orig.didMoveToWindow()
+    func viewDidLoad() {
+        orig.viewDidLoad()
+        
         let gradient = Gradient.init(rawValue: Manager.sharedInstance.notifsGradient)
+        let width = target.view.bounds.width + Manager.sharedInstance.notificationsWidthOffset
+        let height = target.view.bounds.height + Manager.sharedInstance.notificationsHeightOffset
+        let frame = CGRect(origin: target.view.bounds.origin, size: CGSize(width: width, height: height))
         
-        // TODO
-        if(gradientLayer != nil || !isTargetView || gradient == Gradient.none) {return}
-        
-        if let superview = target.superview {
-            
-            gradientLayer = CAGradientLayer()
-            gradientLayer?.frame = superview.bounds
-            
-            switch gradient {
+        switch gradient {
             case .top:
-                gradientLayer?.colors = [UIColor.clear.cgColor, UIColor.white.cgColor]
-                gradientLayer?.locations = [0, 0.1]
+                gradientLayer.frame = frame
+                gradientLayer.colors = [UIColor.clear.cgColor, UIColor.white.cgColor]
+                gradientLayer.locations = [0, 0.1]
+                target.view.layer.mask = gradientLayer
+
             case .bottom:
-                gradientLayer?.colors = [UIColor.white.cgColor, UIColor.clear.cgColor]
-                gradientLayer?.locations = [0.9, 1]
+                gradientLayer.frame = frame
+                gradientLayer.colors = [UIColor.white.cgColor, UIColor.clear.cgColor]
+                gradientLayer.locations = [0.9, 1]
+                target.view.layer.mask = gradientLayer
             default:
                 break
             }
-            
-//            superview.layer.mask = gradientLayer
-        }
-        
     }
     
-    // Temporal solution
-    func layoutSubviews() {
-        orig.layoutSubviews()
-        
-        if let superview = target.superview {
-            gradientLayer?.frame = superview.bounds
-        }
-        
-    }
-    
-    func didMoveToSuperview() {
-        orig.didMoveToSuperview()
-        isTargetView = false
-        
-        let notificationListViewClass = objc_getClass("NCNotificationListView") as! NCNotificationListView.Type
-        guard let superview = target.superview else {
-            return
-        }
-        
-        if(!superview.isKind(of: notificationListViewClass.self)) {
-            isTargetView = true
-            Manager.sharedInstance.notificationsListView = target
-        }
-        
-    }
-    
-    func setFrame(_ frame: CGRect) {
-        
-        // We only want to update one CSCoverSheetViewBase
-        if(isTargetView) {
-            var newFrame = originalFrame
-            if(!Manager.sharedInstance.isEditing) {
-                originalFrame = frame
-                newFrame = frame
-            }
-
-            // Only clips to bounds if user change height
-            target.clipsToBounds = Manager.sharedInstance.notificationsHeightOffset != 0
-            newFrame.size.height += Manager.sharedInstance.notificationsHeightOffset
-                        
-            orig.setFrame(newFrame)
-            gradientLayer?.frame = target.bounds
-        
-        } else {
-            orig.setFrame(frame)
-        }
+    func viewWillLayoutSubviews() {
+        orig.viewWillLayoutSubviews()
+       
+        let width = target.view.bounds.width + Manager.sharedInstance.notificationsWidthOffset
+        let height = target.view.bounds.height + Manager.sharedInstance.notificationsHeightOffset
+        let frame = CGRect(origin: target.view.bounds.origin, size: CGSize(width: width, height: height))
+        gradientLayer.frame = frame
     }
 }
